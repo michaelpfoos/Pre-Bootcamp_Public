f30c59cfe1fe997b54a85680ead4bfc45d7dabbc
michaelpfoos <71546279+michaelpfoos@users.noreply.github.com>
2021-08-24 08:36:40 -0400

    Create Foos_Michael_JS_Tinkering.js

---
+++ Foos_Michael_JS_Tinkering.js
@@ -0,0 +1,10 @@
+function displayIfChildIsAbleToRideTheRollerCoaster(childHeight) {
+    if (childHeight > 52) {
+        console.log('Get on that ride, Kiddo!');
+    } 
+    else {
+        console.log('Sorry Kiddo.  Maybe nex year.');
+    }         
+}
+
+displayIfChildIsAbleToRideTheRollerCoaster(60);
